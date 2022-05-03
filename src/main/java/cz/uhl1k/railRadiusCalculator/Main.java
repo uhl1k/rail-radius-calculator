b6025267d1b15f97c89ca9bbda1922cfc5995673
uhl1k <jankuroman97@gmail.com>
2022-05-03 14:53:49 +0200

    implemented functionality

---
+++ src/main/java/cz/uhl1k/railRadiusCalculator/Main.java
@@ -0,0 +1,13 @@
+package cz.uhl1k.railRadiusCalculator;
+
+import cz.uhl1k.railRadiusCalculator.gui.MainWindow;
+
+/**
+ * Main class of this program.
+ * @author uhl1k (Roman Janků)
+ */
+public class Main {
+  public static void main (String... args) {
+    new MainWindow();
+  }
+}
